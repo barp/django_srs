0967633edfeb6feab2cd25195927e31ccd3f0372
B. Perach <barp12@gmail.com>
2014-06-28 16:19:47 +0300

    added templates

--- srs/urls.py
+++ srs/urls.py
@@ -4,4 +4,5 @@ from srs import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^deck/(?P<pk>[0-9]+)/$', views.DeckView.as_view(), name='deck'),
 ]
--- srs/views.py
+++ srs/views.py
@@ -3,9 +3,15 @@ from django.http import HttpResponse
 from django.views import generic
 from srs.models import Deck
 
+
 class IndexView(generic.ListView):
     template_name = 'srs/deck_index.html'
     context_object_name = 'decks_list'
 
     def get_queryset(self):
         return  Deck.objects.all()
+
+
+class DeckView(generic.DetailView):
+    model = Deck
+    template_name = "srs/deck_detail.html"
