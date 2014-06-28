3c20305d8d9a5c1dcbdbb23d43028e324e05b356
B. Perach <barp12@gmail.com>
2014-06-28 12:10:29 +0300

    changed the models

--- srs/models.py
+++ srs/models.py
@@ -4,7 +4,6 @@ class Deck(models.Model):
     name = models.CharField(max_length=50)
     card_template = models.FileField()
 
-# Create your models here.
 class Card(models.Model):
     decks = models.ManyToManyField(Deck)
     card_name = models.CharField(max_length=50)
@@ -18,6 +17,23 @@ class CardFieldValue(models.Model):
     field = models.ForeignKey(CardField)
     value = models.CharField(max_length=500)
 
-class CardLearningData(models.Model):
-    card = models.ForeignKey(Card)
-    proficiency_level = models.PositiveSmallIntegerField()
+class LearningState(models.Model):
+    parent_deck = models.ForeignKey(Deck)
+    session_number = models.PositiveSmallIntegerField()
+
+class LearningDeck(models.Model):
+    learning_state = models.ForeignKey(LearningState)
+    first_session = models.PositiveSmallIntegerField()
+    CURRENT_DECK = "CUR"
+    IN_PROGRESS_DECK = "PRO"
+    RETIRED_DECK = "RET"
+    DECK_TYPES = (
+        (CURRENT_DECK, "Current Deck"),
+        (IN_PROGRESS_DECK, "In Progress Deck"),
+        (RETIRED_DECK, "Retired Deck")
+    )
+    deck_type = models.CharField(max_length=3, choices=DECK_TYPES, default=CURRENT_DECK)
+
+class CardLearningInstance(models.Model):
+    actual_card = models.ForeignKey(Card)
+    current_learning_deck = models.ForeignKey(LearningDeck)
