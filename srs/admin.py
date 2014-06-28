e477546d0c2ba8c395157da4cf032442eec756e0
B. Perach <barp12@gmail.com>
2014-06-28 14:04:14 +0300

    reformat admin file

--- srs/admin.py
+++ srs/admin.py
@@ -2,16 +2,20 @@ from django.contrib import admin
 from srs.models import Deck, CardField, Card, CardFieldValue
 import django.forms as forms
 
+
 class CardFieldsAdmin(admin.TabularInline):
     model = CardField
     extra = 5
 
+
 class DeckAdmin(admin.ModelAdmin):
     inlines = [CardFieldsAdmin]
+
 
 class CardFieldValueAdmin(admin.TabularInline):
     model = CardFieldValue
     extra = 5
+
 
 class CardAdmin(admin.ModelAdmin):
     inlines = [CardFieldValueAdmin]
